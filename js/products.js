3440f53d9fa0f33fd15982c6675cc8b55add59e8
GonzalezFabian <tenoch86@msn.com>
2023-03-17 14:41:49 -0600

    ultimo

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Trek",
+    precio: 35000,
+    img:
+      "https://trek.scene7.com/is/image/TrekBicycleProducts/Marlin8_21_34649_A_Portrait?$responsive-pjpg$&cache=on,on&wid=1440&hei=1080",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Giant",
+    precio: 34400,
+    img:
+      "https://bikestore.com.mx/blog/wp-content/uploads/2021/02/giant-stp-2021.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Alubike",
+    precio: 15000,
+    img:
+      "https://scontent.fmid3-1.fna.fbcdn.net/v/t39.30808-6/240591704_10156981076062614_8679511468677043554_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=YL3HEPe-oNgAX_tinI0&_nc_ht=scontent.fmid3-1.fna&oh=00_AfCVbUbwXsTcxeazxtNFXFUgPg8LlQzYPh2uyHedpJYGWQ&oe=6419344B",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Santa Cruz",
+    precio: 250000,
+    img:
+      "https://www.santacruzbicycles.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fsantacruzbikesstatic%2Fba7ecaca-ad56-4c1b-bc42-6d1abf8d593a_Roll%2BUp%2B-%2BPhoto.jpg%3Fauto%3Dcompress%2Cformat&w=2100&q=75",
+    cantidad: 1,
+  },
+];
